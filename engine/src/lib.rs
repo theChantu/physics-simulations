107a3fb794370ce647fdeb7660eced3b6d3e8f43
Jonathan Small <29470391+theChantu@users.noreply.github.com>
2026-02-04 22:22:47 -0500

    update workflow

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,12 +1,7 @@
 mod engine;
+use wasm_bindgen::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = engine::add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn add(left: u64, right: u64) -> u64 {
+    engine::add(left, right)
 }
--- src/routes/particle/+page.svelte
+++ src/routes/particle/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { Plus, Minus } from 'lucide-svelte';
-	import * as wasmlib from '../../../engine/pkg';
-	import wasm from 'vite-plugin-wasm';
+	import init from '../../../engine/pkg/engine';
 
 	const WIDTH = 1000;
 	const HEIGHT = 600;
@@ -11,8 +10,8 @@
 	let animationFrameId: ReturnType<typeof requestAnimationFrame>;
 
 	onMount(() => {
-		wasmlib.default().then(() => {
-			console.log('WASM loaded');
+		init().then(() => {
+			console.log('WASM module initialized');
 		});
 
 		context = canvas.getContext('2d');
